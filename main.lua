bcdc5d8f7a9edbe50d6f5b9647bb429f6b79ef63
Pragsz868 <cartoonsw630@gmail.com>
2025-07-02 22:10:25 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -17,24 +17,35 @@ local MainTab = UI.New({
 
 MainTab.TextField({
     Text = "Target Username",
+    Placeholder = "Enter player's username",
     Callback = function(value)
-        targetPlayerName = value
+        if value ~= "" then
+            targetPlayerName = value
+        else
+            UI.Banner("Please enter a valid username")
+        end
     end,
     Menu = {
         Information = function()
-            UI.Banner("Enter the username of the target player")
+            UI.Banner("Enter the username of the player you want to fake donate to")
         end
     }
 })
 
 MainTab.TextField({
     Text = "Robux Amount",
+    Placeholder = "Enter Robux (e.g. 100)",
     Callback = function(value)
-        robuxAmount = tonumber(value) or 1000000
+        local num = tonumber(value)
+        if num then
+            robuxAmount = num
+        else
+            UI.Banner("Please enter a valid number")
+        end
     end,
     Menu = {
         Information = function()
-            UI.Banner("Enter the amount of Robux to transfer")
+            UI.Banner("Enter how much Robux to show in the donation popup")
         end
     }
 })
@@ -51,60 +62,60 @@ MainTab.Button({
             end
             return formatted
         end
-        
+
         local Players = game:GetService("Players")
         local TweenService = game:GetService("TweenService")
         local localPlayer = Players.LocalPlayer
-        
+
         if localPlayer.PlayerGui:FindFirstChild("UITemplates") and localPlayer.PlayerGui.UITemplates:FindFirstChild("donationPopup") then
-            local donationText = targetPlayerName.." DONATED "..formatNumber(robuxAmount).." TO YOU!"
-            
+            local donationText = targetPlayerName .. " DONATED " .. formatNumber(robuxAmount) .. " TO YOU!"
+
             local ScreenGui = localPlayer.PlayerGui:FindFirstChild("ScreenGui")
             if not ScreenGui then
                 ScreenGui = Instance.new("ScreenGui")
                 ScreenGui.Name = "ScreenGui"
                 ScreenGui.Parent = localPlayer.PlayerGui
-                
+
                 local popups = Instance.new("Frame")
                 popups.Name = "Popups"
                 popups.BackgroundTransparency = 1
                 popups.Size = UDim2.new(1, 0, 1, 0)
                 popups.Parent = ScreenGui
             end
-            
+
             local clone = localPlayer.PlayerGui.UITemplates.donationPopup:Clone()
             clone.Message.Text = donationText
             clone.Transparency = 1
             clone.UIScale.Scale = 0
             clone.Parent = ScreenGui.Popups
-            
+
             if localPlayer:FindFirstChild("leaderstats") and localPlayer.leaderstats:FindFirstChild("Raised") then
                 localPlayer.leaderstats.Raised.Value = localPlayer.leaderstats.Raised.Value + robuxAmount
             end
-            
+
             TweenService:Create(clone, TweenInfo.new(0.5, Enum.EasingStyle.Quint), {
                 Transparency = 0
             }):Play()
-            
+
             local easeStyle = Enum.EasingStyle.Back or Enum.EasingStyle.Quint
-            
+
             TweenService:Create(clone.UIScale, TweenInfo.new(0.3, easeStyle), {
                 Scale = 1
             }):Play()
-            
+
             TweenService:Create(clone.Message, TweenInfo.new(1, Enum.EasingStyle.Quint), {
                 MaxVisibleGraphemes = #donationText
             }):Play()
-            
+
             task.delay(4, function()
                 TweenService:Create(clone, TweenInfo.new(0.25, Enum.EasingStyle.Quint), {
                     Transparency = 1
                 }):Play()
-                
+
                 TweenService:Create(clone.UIScale, TweenInfo.new(0.5, easeStyle), {
                     Scale = 0
                 }):Play()
-                
+
                 task.delay(0.5, function()
                     clone:Destroy()
                 end)
